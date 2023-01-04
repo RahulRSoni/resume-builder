e7d6102c204f8757a4db1934ce37803781ec5a0b
vinay6199 <awesomechowdary619@gmail.com>
2023-01-04 23:22:34 +0530

    Added to vinay

--- src/Componant/Home.js
+++ src/Componant/Home.js
@@ -1,21 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
-import Img1 from './Assets/img-1.svg';
-import Img2 from './Assets/img2.svg';
-import Img3 from './Assets/img3.svg';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import IconButton from '@mui/material/IconButton';
-import { Link } from 'react-router-dom';
-import CheckIcon from '@mui/icons-material/Check';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+import Img1 from "./Assets/img-1.svg";
+import Img2 from "./Assets/img2.svg";
+import Img3 from "./Assets/img3.svg";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
+import CheckIcon from "@mui/icons-material/Check";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 export default function Home() {
-
   const style = {
     textDecoration: "none",
     margin: "15px",
@@ -23,17 +20,21 @@ export default function Home() {
 
   return (
     <>
-      <Box sx={{ display: "flex", mr: 6, ml: 6 }} >
+      <Box sx={{ display: "flex", mr: 6, ml: 6 }}>
         <Grid container rows={{ xs: 12, sm: 12, md: 6 }} rowSpacing={2}>
           <Grid item sm={12} md={6} sx={{ p: 2 }}>
-            <img src={Img1} className="App-logo" alt="logo" height='400vh' />
+            <img src={Img1} className="App-logo" alt="logo" height="400vh" />
           </Grid>
           <Grid item sm={12} md={6} sx={{ p: 2, mt: 6 }}>
-            <Typography align="center" variant='h2' >
+            <Typography align="center" variant="h2">
               Made Your Own Online Resume
             </Typography>
-            <Typography variant='body1' align="justify">
-              You create your professional resume in a quick and easy way. 15+ all type of templates like Professional, Tradisnal,and Morden way. All of which can be customized to your liking. Fast and Easy to Use Our resume builder lets you easily and quickly create a resume using our resume wizard.
+            <Typography variant="body1" align="justify">
+              You create your professional resume in a quick and easy way. 15+
+              all type of templates like Professional, Tradisnal,and Morden way.
+              All of which can be customized to your liking. Fast and Easy to
+              Use Our resume builder lets you easily and quickly create a resume
+              using our resume wizard.
             </Typography>
             <IconButton color="primary" component="label">
               <Link to="/templates">
@@ -44,14 +45,17 @@ export default function Home() {
         </Grid>
       </Box>
 
-      <Box sx={{ display: "flex", ml: 6 }} >
+      <Box sx={{ display: "flex", ml: 6 }}>
         <Grid container rows={{ xs: 12, sm: 12, md: 6 }} rowSpacing={2}>
           <Grid item sm={12} md={6} sx={{ p: 2, mt: 15 }}>
-            <Typography align="center" variant='h2' >
+            <Typography align="center" variant="h2">
               Easy To Get Your Resume.
             </Typography>
-            <Typography variant='body1' align="justify">
-              Ones you created your resume, Our text editor has everything you need to customize your resume. Download your resume and send it straight to the hiring manager. Whatever format you choose, we’ve got you covered.
+            <Typography variant="body1" align="justify">
+              Ones you created your resume, Our text editor has everything you
+              need to customize your resume. Download your resume and send it
+              straight to the hiring manager. Whatever format you choose, we’ve
+              got you covered.
             </Typography>
             <IconButton color="primary" component="label">
               <Link to="/myresume">
@@ -59,56 +63,51 @@ export default function Home() {
               </Link>
             </IconButton>
           </Grid>
-          <Grid item sm={12} md={6} sx={{ p: 2 }} >
-            <img src={Img2} className="App-logo" alt="logo" height='400vh' />
+          <Grid item sm={12} md={6} sx={{ p: 2 }}>
+            <img src={Img2} className="App-logo" alt="logo" height="400vh" />
           </Grid>
         </Grid>
       </Box>
 
-      <Box sx={{ display: "flex", mr: 6, ml: 6 }} >
+      <Box sx={{ display: "flex", mr: 6, ml: 6 }}>
         <Grid container rows={{ xs: 12, sm: 12, md: 6 }} rowSpacing={2}>
           <Grid item sm={12} md={6} sx={{ p: 2, mt: 10 }}>
-            <img src={Img3} className="App-logo" alt="logo" height='400vh' />
+            <img src={Img3} className="App-logo" alt="logo" height="400vh" />
           </Grid>
           <Grid item sm={12} md={6} sx={{ p: 2, mt: 6 }}>
-            <Typography align="center" variant='h2' >
+            <Typography align="center" variant="h2">
               Chose Your Template & Build Your Resume
             </Typography>
-            <Typography variant='h6' align="justify">
-              <IconButton color="primary" component="label">
-                <CheckIcon />
-              </IconButton>
+            <Typography variant="h6" align="justify">
+              <CheckIcon />
               10+ Professional Resume Templates
             </Typography>
-            <Typography variant='body1' align="justify">
-              Choose from over tem modern and professional templates. All of which can be customized to your liking.
+            <Typography variant="body1" align="justify">
+              Choose from over tem modern and professional templates. All of
+              which can be customized to your liking.
             </Typography>
-            <Typography variant='h6' align="justify">
-              <IconButton color="primary" component="label">
-                <CheckIcon />
-              </IconButton>
+            <Typography variant="h6" align="justify">
+              <CheckIcon />
               Fast and Easy to Use
             </Typography>
-            <Typography variant='body1' align="justify">
-              Our free resume builder lets you easily and quickly create a resume using our resume wizard.
+            <Typography variant="body1" align="justify">
+              Our free resume builder lets you easily and quickly create a
+              resume using our resume wizard.
             </Typography>
-            <Typography variant='h6' align="justify">
-              <IconButton color="primary" component="label">
-                <CheckIcon />
-              </IconButton>
+            <Typography variant="h6" align="justify">
               Robust Text Editor
             </Typography>
-            <Typography variant='body1' align="justify">
-              Our text editor has everything you need to customize your resume. Choose different fonts, sizes, bullets and much more.
+            <Typography variant="body1" align="justify">
+              Our text editor has everything you need to customize your resume.
+              Choose different fonts, sizes, bullets and much more.
             </Typography>
-            <Typography variant='h6' align="justify">
-              <IconButton color="primary" component="label">
-                <CheckIcon />
-              </IconButton>
+            <Typography variant="h6" align="justify">
+              <CheckIcon />
               Download your resume as PDF
             </Typography>
-            <Typography variant='body1' align="justify">
-              Download your resume and send it straight to the hiring manager. Whatever format you choose, we’ve got you covered.
+            <Typography variant="body1" align="justify">
+              Download your resume and send it straight to the hiring manager.
+              Whatever format you choose, we’ve got you covered.
             </Typography>
             <IconButton color="primary" component="label">
               <Link to="/templates">
@@ -119,11 +118,10 @@ export default function Home() {
         </Grid>
       </Box>
 
-        <Box sx={{ bgcolor: '#cfe8fc', height: 250}}>
-
+      <Box sx={{ bgcolor: "#cfe8fc", height: 250 }}>
         <List>
-            <ListItem>
-            <Link to="/templates" underline="none" style={style} >
+          <ListItem>
+            <Link to="/templates" underline="none" style={style}>
               Resume Templates
             </Link>
 
@@ -134,10 +132,10 @@ export default function Home() {
             <Link to="/about" underline="none" style={style}>
               About Us
             </Link>
-            </ListItem>,
+          </ListItem>
+          ,
         </List>
-        </Box>
-
+      </Box>
     </>
   );
 }

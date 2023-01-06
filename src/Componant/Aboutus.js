79d16cab41209464b7dc64bd10af855a77cd4d93
vinay6199 <awesomechowdary619@gmail.com>
2023-01-06 21:31:47 +0530

    share icon design done

--- src/Componant/Aboutus.js
+++ src/Componant/Aboutus.js
@@ -3,6 +3,14 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import Aboutimg from "./Assets/Aboutimg.jpg";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
+
 
 
 const Aboutus = () => {
@@ -15,15 +23,43 @@ const Aboutus = () => {
               Resume <br></br>
               Builder
             </Typography>
-            <Typography variant="body1" align="justify">
+            <Typography variant="body1" align="justify" sx={{ fontWeight: 'medium' }}>
               The job market is more competitive today than it’s ever been before. Whenever you apply for an open position, it’s safe to assume that dozens if not hundreds of other people are applying for the exact same position as well.
               You’ll need an excellent resume in order to stand out from the competition. But writing effective resumes can be hard — you must elaborate enough on your background and skill set to impress prospective employers, yet you also need to be concise enough to make sure a hiring manager adds you to the “yes” pile quickly.
               You should also be concerned about your resume’s design. Many employers now use applicant tracking system (ATS) software to automate the initial stage of the hiring process. If the formatting of your resume isn’t optimized for such software, it might get filtered out before it even reaches the person who decides whether or not you get an interview.
-              Thankfully, there are services available that can take care of these difficult aspects of resume writing for you. In addition to writing and designing your resume, these providers often offer cover letter writing, LinkedIn profile optimization, interview coaching, and other valuable career services.
+              {/* Thankfully, there are services available that can take care of these difficult aspects of resume writing for you. In addition to writing and designing your resume, these providers often offer cover letter writing, LinkedIn profile optimization, interview coaching, and other valuable career services. */}
             </Typography>
-            <Typography align="left" variant="h2" sx={{ fontWeight: 'medium' }}>
+            <br></br>
+            <br></br>
+            <Typography align="left" variant="h4" sx={{ fontWeight: 'medium' }}>
               Share with your friends
             </Typography>
+
+            <IconButton color="primary" component="label"sx={{padding:2}}>
+              <Link to="/templates">
+                <WhatsAppIcon />   
+              </Link>
+            </IconButton>
+            <IconButton color="primary" component="label" sx={{padding:2}}>
+              <Link to="/https://www.facebook.com/">
+                <LinkedInIcon/> 
+              </Link>
+            </IconButton>
+            <IconButton color="primary" component="label" sx={{padding:2}}>
+              <Link to="/templates">
+                <FacebookIcon/> 
+              </Link>
+            </IconButton>
+            <IconButton color="primary" component="label"sx={{padding:3}}>
+              <Link to="/templates">
+          <TwitterIcon/> 
+          <IconButton color="primary" component="label" sx={{padding:3}}>
+              <Link to="/templates">
+                 <EmailIcon/>
+              </Link>
+            </IconButton>
+              </Link>
+            </IconButton>
           </Grid>
           <Grid item sm={12} md={6} mt={20} sx={{ p: 20 }}>
             <img src={Aboutimg} className="App-logo" alt="logo" height="300vh" />

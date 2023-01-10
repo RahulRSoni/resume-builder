258bc0a21acdf2fd1b4761ab29dfe4cfd9a76ebf
vinay6199 <awesomechowdary619@gmail.com>
2023-01-10 21:56:27 +0530

    structure components created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Componant/Home';
 import Aboutus from './Componant/Aboutus';
 import Reactdark from './Componant/Reactdark';
+import Myresume from './Componant/Myresume';
+
 
 
 
@@ -11,15 +13,16 @@ function App() {
   <div>
     
       <Navbar/> 
-      <Reactdark/>
+      {/* <Reactdark/> */}
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/templates" element={<Navbar />}></Route>
-        <Route exact path="/myresume" element={<Navbar />}></Route>
+        <Route exact path="/myresume" element={<Myresume/>}></Route>
         <Route exact path="/about" element={<Aboutus />}></Route>
+      
+
       </Routes>
       
-   
   </div>
   ) 
 }

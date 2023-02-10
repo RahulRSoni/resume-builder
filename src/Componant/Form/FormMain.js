b3d9ac612973b2ea5d924a22cd943c56df49d598
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2023-02-10 18:26:48 +0530

    s

--- src/Componant/Form/FormMain.js
+++ src/Componant/Form/FormMain.js
@@ -5,6 +5,7 @@ import { useState } from "react"
 import PersonalInfo from "./PersonalInfo"
 import WorkExp from "./WorkEx"
 import EduInfo from "./EduInfo"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SkillNLanguages from "./SkillNLanguages"
 import OtherInfo from "./OtherInfo"
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
@@ -62,29 +63,41 @@ const FormMain = () => {
                 level: ""
             }],
 
-            language:[{
+            language: [{
                 languageName: "",
-                level:""
+                level: ""
             }],
-            link1:"",
-            link2:"",
-            link3:"",
-            link4:'',
-            link5:'',
-            link6:'',
-            hobbies:'',
+            link1: "",
+            link2: "",
+            link3: "",
+            link4: '',
+            link5: '',
+            link6: '',
+            hobbies: '',
 
-            references:[{
-                refName:"",
-                RefDetail:'',
-                RefEmail:'',
-                RefMobile:'',
+            references: [{
+                refName: "",
+                RefDetail: '',
+                RefEmail: '',
+                RefMobile: '',
             }],
-            
+
         }
     });
 
-    const [value, setValue] = useState("1");
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 600,
+                md: 980,
+                lg: 1200,
+                xl: 1536,
+            },
+        },
+    });
+
+    const [value, setValue] = useState("0");
     const [buttonText, setButtonText] = useState("Next")
 
     const changeButtonText = (value) => {
@@ -110,7 +123,6 @@ const FormMain = () => {
         let str = String(genValue - 1)
         setValue(str)
         changeButtonText(value)
-
     }
 
     const handleChange = (e, newValue) => {
@@ -118,52 +130,57 @@ const FormMain = () => {
     }
 
 
-    
+
     return (
-        <Grid container display="flex" direction="row" justifyContent="center" alignItems="flex-start" sx={{ m: 1, p: 1 }} >
-            <TabContext value={value} >
-                <Grid item md={2}>
-                    <Box sx={{ borderRight: 2, borderColor: "divider", width: "200px" }}>
-                        <TabList aria-label="Tab Navigation" orientation="vertical" onChange={handleChange} TabIndicatorProps={{
-                            sx: { bgcolor: "red", height: "5px", left: "0px", width: "4px", textTransform: "capitalize" }
-                        }}>
-                            <Tab label="Personal Information" value="1" sx={{ textTransform: "capitalize" }} />
-                            <Tab label="Work Experience" value="2" sx={{ textTransform: "capitalize" }} />
-                            <Tab label="Education" value="3" sx={{ textTransform: "capitalize" }} />
-                            <Tab label="Skills & Languages" value="4" sx={{ textTransform: "capitalize" }} />
-                            <Tab label="Other Information" value="5" sx={{ textTransform: "capitalize" }} />
-                        </TabList>
-                    </Box>
-                </Grid>
-                <Grid container item md={10} display="flex" justifyContent="center" >
-                    <Paper elevation={10} >
-                        <Grid item>
-                            <Box sx={{ width: 700, maxHeight: "100%", maxWidth: "100%", }}>
-                                <FormProvider {...formMethod}><form id="nextButton" onSubmit={formMethod.handleSubmit(handleNext)}>
-                                    <TabPanel value="1"><PersonalInfo /></TabPanel>
-                                    <TabPanel value="2"><WorkExp /></TabPanel>
-                                    <TabPanel value="3"><EduInfo /></TabPanel>
-                                    <TabPanel value="4"><SkillNLanguages /></TabPanel>
-                                    <TabPanel value="5"><OtherInfo /></TabPanel>
-                                </form></FormProvider>
-                            </Box>
-                        </Grid>
+        <ThemeProvider theme={theme}>
+            <Grid container display="flex" direction="row" justifyContent="center" alignItems="flex-start" sx={{ m: 1 }} >
+                <TabContext value={value} >
+                    <Grid item md={2}>
+                        <Box sx={{ borderRight: 2, borderColor: "divider", width: "200px" }}>
+                            <TabList aria-label="Tab Navigation" orientation={ `theme.breakpoints.keys[2] === "md" ? "vertical" : ""`} onChange={handleChange} TabIndicatorProps={{
+                                sx: { bgcolor: "red", height: "5px", left: "0px", width: "4px", textTransform: "capitalize" }
+                            }}>
+                                <Tab label="Personal Information" value="0" sx={{ textTransform: "capitalize" }} />
+                                <Tab label="Work Experience" value="1" sx={{ textTransform: "capitalize" }} />
+                                <Tab label="Education" value="2" sx={{ textTransform: "capitalize" }} />
+                                <Tab label="Skills & Languages" value="3" sx={{ textTransform: "capitalize" }} />
+                                <Tab label="Other Information" value="4" sx={{ textTransform: "capitalize" }} />
+                            </TabList>
+                        </Box>
+                    </Grid>
+                    <Grid container item md={10} display="flex" justifyContent="center" >
+                        <Paper elevation={8} >
+                            <Grid item>
+                                <Box sx={{ width: 700, maxHeight: "100%", maxWidth: "100%", }}>
+                                    <FormProvider {...formMethod}>
+                                        <form id="nextButton" onSubmit={formMethod.handleSubmit(handleNext)}>
+                                            <TabPanel value="0"><PersonalInfo /></TabPanel>
+                                            <TabPanel value="1"><WorkExp /></TabPanel>
+                                            <TabPanel value="2"><EduInfo /></TabPanel>
+                                            <TabPanel value="3"><SkillNLanguages /></TabPanel>
+                                            <TabPanel value="4"><OtherInfo /></TabPanel>
+                                        </form>
+                                    </FormProvider>
+                                </Box>
+                            </Grid>
 
-                        <Grid container item direction="row" justifyContent="space-between" alignItems="center" mx={1} p={2}>
-                            <Button variant="text" startIcon={<ArrowBackIosIcon />} disabled={value <= 1 ? true : false} onClick={() => handleBack()}>
-                                Back
-                            </Button>
-                            <Button variant="text" type="submit" form="nextButton" endIcon={<ArrowForwardIosIcon />} disabled={value > 5 ? true : false} >
-                                {buttonText}
-                            </Button>
-                        </Grid>
+                            <Grid container item direction="row" justifyContent="space-between" alignItems="center" mx={1} p={2}>
+                                <Button color="inherit" startIcon={<ArrowBackIosIcon />} disabled={value === "0"} onClick={() => handleBack()}>
+                                    Back
+                                    {console.log(theme.breakpoints)}
+                                </Button>
+                                <Button type="submit" form="nextButton" endIcon={<ArrowForwardIosIcon />} disabled={value > 5 ? true : false} >
+                                    {buttonText}
+                                </Button>
+                            </Grid>
 
-                    </Paper>
+                        </Paper>
 
-                </Grid>
+                    </Grid>
 
-            </TabContext>
-        </Grid>
+                </TabContext>
+            </Grid>
+        </ThemeProvider>
     )
 }
 
